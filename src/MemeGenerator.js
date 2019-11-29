de62ca8e6473733655950fd98ca8532b95556b7b
Andrea T <tostonar@gmail.com>
2019-11-29 12:04:09 -0500

    first commit

---
+++ src/MemeGenerator.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+class MemeGenerator extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <h2>Meme Generator Section</h2>
+    )
+  }
+}
+
+export default MemeGenerator
